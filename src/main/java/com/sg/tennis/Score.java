80797c17946163ed59966cce276a024b8a34f6fc
Abdelhadi BENNAJI <abdelhadi.bennaji@gmail.com>
2020-07-26 23:02:38 +0200

    Test If the player who has the ADVANTAGE win the point he win the game : OK

--- src/main/java/com/sg/tennis/Score.java
+++ src/main/java/com/sg/tennis/Score.java
@@ -12,6 +12,6 @@ public enum Score {
     public Score getNext() {
         return this.ordinal() < Score.values().length - 1
                 ? Score.values()[this.ordinal() + 1]
-                : null;
+                : WIN;
     }
 }
